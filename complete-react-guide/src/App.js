f8066151f1894f77290a123adbeb64a9482ec166
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-06-11 15:17:33 -0400

    Added Method References

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -13,20 +13,20 @@ const App = props => {
       otherState: 'some other value'
   });
 
-  const [otherState, setOtherState] = useState('some other value');
+  const [otherState] = useState('some other value');
 
   console.log(personsState, otherState);
   
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log("This button has been clicked!");
     // DONT DO THIS personsState.persons[0].name = 'Borisella';
     setPersonsState({
       persons: [
-        {name: 'Borisella', age: 28},
+        {name: newName, age: 28},
         {name: 'Alan', age: 28},
         {name: 'Zhi', age: 34}
       ],
-      otherState: setOtherState
+      otherState: otherState
     });
   };
 
@@ -34,10 +34,17 @@ const App = props => {
     <div className="App">
       <h1>This is a React Application! I'm feeling good.</h1>
       <p>Awesome! This is really working.</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name}  age={personsState.persons[2].age}/>
+      <button onClick={() => switchNameHandler('Borisella!!')}>Switch Name</button>
+      <Person 
+      name={personsState.persons[0].name} 
+      age={personsState.persons[0].age}/>
+      <Person 
+      name={personsState.persons[1].name} 
+      age={personsState.persons[1].age}
+      click={switchNameHandler.bind(this, 'Boris!')}>My Hobbies: Racing</Person>
+      <Person 
+      name={personsState.persons[2].name}  
+      age={personsState.persons[2].age}/>
     </div>
     );
   }
