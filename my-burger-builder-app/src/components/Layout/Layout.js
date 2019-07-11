93569ca0c22a3df859d37798d07e61aee6aa2a01
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-07-11 13:23:53 -0400

    Updated Layout Component

--- my-burger-builder-app/src/components/Layout/Layout.js
+++ my-burger-builder-app/src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import styles from "./Layout.css";
 import Navbar from "../Navigation/NavBar/NavBar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Aux>
     <Navbar />
+    <SideDrawer />
     <main className={styles.Content}>{props.children}</main>
   </Aux>
 );
